007f09d30ee159309719dc0af68fe03deef24e89
rraven809 <sem110518@gmail.com>
2022-09-28 23:40:21 +0300

    Дописан класс QueueR

--- prj.lab/queuer/queuer.cpp
+++ prj.lab/queuer/queuer.cpp
@@ -23,7 +23,7 @@ int QueueR::top()
 void QueueR::push(const int& val)
 {
 	_size += 1;
-	if (_size == 0) {
+	if (_size == 1) {
 		Node* nd = new Node(val, nullptr);
 		first = nd;
 		last = nd;
@@ -33,26 +33,17 @@ void QueueR::push(const int& val)
 	if (first->get_value() > val) {
 		Node* nd = new Node(val, first);
 		first = nd;
+		return;
 	}
-
-
-
-
 
 	Node* temp = first;
 	while (temp->get_value() > val) {
-
+		temp = temp->next;
 	}
 
-
-
-
-
-
-	Node* nd = new Node(val, nullptr);
+	Node* nd = new Node(val, temp);
 	last->next = nd;
 	last = nd;
-	return;
 }
 
 void QueueR::pop()
